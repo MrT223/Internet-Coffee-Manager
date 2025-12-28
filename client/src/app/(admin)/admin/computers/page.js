e0ee97dbbd78d006ec45ddaf0329dd8bbfd850e0
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2025-12-28 22:35:10 +0700

    fix bug deloy

--- client/src/app/(admin)/admin/computers/page.js
+++ client/src/app/(admin)/admin/computers/page.js
@@ -1,11 +1,14 @@
 'use client';
+import { Suspense } from 'react';
 import ComputerMap from '@/components/user/ComputerMap';
 
 export default function AdminComputersPage() {
     return (
         <div className="h-full flex flex-col">
             <div className="flex-1 overflow-hidden p-4">
-                <ComputerMap />
+                <Suspense fallback={<div className="text-white text-center mt-10">Đang tải bản đồ...</div>}>
+                    <ComputerMap />
+                </Suspense>
             </div>
         </div>
     );
